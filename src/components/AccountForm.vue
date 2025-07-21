5990b0fd109f858330649d2599f76a1369a11a11
Mihail Chepovskiy <contact@salephpscripts.com>
2025-07-22 02:52:32 +0600

    AccountItem component added

--- src/components/AccountForm.vue
+++ src/components/AccountForm.vue
@@ -6,49 +6,12 @@
       Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;
     </p>
 
-    <div
-      v-for="(account, index) in accounts"
-      :key="account.id"
-      class="">
-      <!-- Метка -->
-      <input
-        class=""
-        placeholder="Метки"
-        :value="account.labels.map(l => l.text).join('; ')"
-      />
-
-      <!-- Тип записи -->
-      <select
-        class=""
-        v-model="account.type"
-      >
-        <option value="Локальная">Локальная</option>
-        <option value="LDAP">LDAP</option>
-      </select>
-
-      <!-- Логин -->
-      <input
-        class=""
-        placeholder="Логин"
-        v-model="account.login"
-      />
-
-      <!-- Пароль -->
-      <input
-        v-if="account.type === 'Локальная'"
-        type="password"
-        class=""
-        placeholder="Пароль"
-        v-model="account.password"
-      />
-
-      <!-- Удаление -->
-      <button @click="removeAccount(account.id)" class="">🗑️</button>
-    </div>
+    <AccountItem />
   </div>
 </template>
 
 <script setup lang="ts">
+import AccountItem from '../components/AccountItem.vue'
 import { useAccountsStore } from '@/stores/useAccountsStore'
 import type { Account } from '@/types/Account'
 
---
+++ src/components/AccountItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div
+        v-for="(account, index) in accounts"
+        :key="account.id"
+        class="">
+        <!-- Метка -->
+        <input
+            class=""
+            placeholder="Метки"
+            :value="account.labels.map(l => l.text).join('; ')"
+        />
+
+        <!-- Тип записи -->
+        <select
+            class=""
+            v-model="account.type"
+        >
+            <option value="Локальная">Локальная</option>
+            <option value="LDAP">LDAP</option>
+        </select>
+
+        <!-- Логин -->
+        <input
+            class=""
+            placeholder="Логин"
+            v-model="account.login"
+        />
+
+        <!-- Пароль -->
+        <input
+            v-if="account.type === 'Локальная'"
+            type="password"
+            class=""
+            placeholder="Пароль"
+            v-model="account.password"
+        />
+
+        <!-- Удаление -->
+        <button @click="removeAccount(account.id)" class="">🗑️</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useAccountsStore } from '@/stores/useAccountsStore'
+import type { Account } from '@/types/Account'
+
+const { accounts, removeAccount } = useAccountsStore()
+
+</script>
